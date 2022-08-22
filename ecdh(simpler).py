cc07230a7dfc388802abb4b2ded3479b3e8b9288
Jack Sovereign <86751863+jacc-sovv@users.noreply.github.com>
2022-08-21 20:08:03 -0600

    Create ecdh(simpler).py

---
+++ ecdh(simpler).py
@@ -0,0 +1,25 @@
+# Potential problem : no seed to set? Completely 'random'?
+from tinyec import registry
+import secrets
+def compress(pubKey):
+    return hex(pubKey.x) + hex(pubKey.y % 2)[2:]
+
+curve = registry.get_curve('brainpoolP256r1')
+
+alicePrivKey = secrets.randbelow(curve.field.n) #In diagram: da
+alicePubKey = alicePrivKey * curve.g        # QA = da * G
+print("Alice public key:", compress(alicePubKey))
+
+bobPrivKey = secrets.randbelow(curve.field.n)   #r
+bobPubKey = bobPrivKey * curve.g            #R * G
+print("Bob public key:", compress(bobPubKey))
+
+print("Now exchange the public keys (e.g. through Internet)")
+
+aliceSharedKey = alicePrivKey * bobPubKey       #mathematically equal to r * QA
+print("Alice shared key:", compress(aliceSharedKey))
+
+bobSharedKey = bobPrivKey * alicePubKey     #r * QA
+print("Bob shared key:", compress(bobSharedKey))
+
+print("Equal shared keys:", aliceSharedKey == bobSharedKey)
